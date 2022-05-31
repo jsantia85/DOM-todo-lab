b2ce64bcdcc47223dd5ded3fc2544495cf7c4f19
JASantia85 <JuanASantiago1058@gmail.com>
2022-05-31 17:59:15 -0400

    implements event listener for submitBtn

--- js/app.js
+++ js/app.js
@@ -4,6 +4,15 @@ const submitBtn = document.querySelector("#submit-button")
 const todoList = document.querySelector("#todo-list")
 
 /*---------------------- Event Listeners ----------------------*/
-
+submitBtn.addEventListener('click', function(evt) {
+  if(input.value === '') return
+  //creating new li element
+  const newLiItem = document.createElement('li')
+  // console.log(newLiItem)
+  newLiItem.textContent = input.value
+  // console.log(newLiItem)
+  todoList.appendChild(newLiItem)
+  input.value = ''
+})
 
 
