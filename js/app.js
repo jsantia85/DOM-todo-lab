159c59dc627d26dc37adadb4f03b88129f571717
JASantia85 <JuanASantiago1058@gmail.com>
2022-05-31 17:24:42 -0400

    sets up input, submitBtn, and todoList cached element ref

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*----------------- Cached Element References -----------------*/
+const input = document.querySelector("#input")
+const submitBtn = document.querySelector("#submit-button")
+const todoList = document.querySelector("#todo-list")
+
+/*---------------------- Event Listeners ----------------------*/
+
+
+
